dd31c76ed06aff14faacc538cbb6f06c4fef61b3
AKBANGOURA <bangourakallaa@gmail.com>
2025-12-23 15:58:02 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+SUPABASE_URL = "https://jxmzinqfbgbzxdsxfqot.supabase.co"
+# Remplace par la clé 'anon' 'public' qui se trouve juste en dessous de l'URL dans tes paramètres Supabase
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4bXppbnFmYmdienhkc3hmcW90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNzc3NzcsImV4cCI6MjA4MTk1Mzc3N30.1wIMS1yN898WNjC_TnQDm5LGpNM02O97nLIRzgNAPJM"
+GOOGLE_API_KEY = "AIzaSyDeWrU9SBtlIA0_aYt4kfnPLq1EtlSFiMM"
